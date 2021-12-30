80665f3e7e9b4f5215b14fd3d2cd64d1485a577b
scottly426 <lyscott123@gmail.com>
2021-12-29 16:56:38 -0800

    added form input component and styles

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import {Switch, Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
+import SignInAndSignOut from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx';
 
+import Header from './components/header/header.component.jsx';
 
 import './App.css';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={HomePage}/>
-      <Route path='/shop' component={ShopPage}/>
-    </Switch>
-    
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignOut}/>
+      </Switch>
+    </div>
   );
 }
 
