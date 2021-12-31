b54644d84d99da6af58ce76bfd0aaa495c4fa413
scottly426 <lyscott123@gmail.com>
2021-12-31 12:05:50 -0800

    implemented firebase utils, included ability to store authenticated user into firestore database

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {Switch, Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
 import SignInAndSignOut from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 import Header from './components/header/header.component.jsx';
 
@@ -22,8 +22,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this. unsubScribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user})
+    this. unsubScribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id:snapShot.id,
+              ...snapShot.data()
+            }
+          })
+          console.log(this.state);
+        })
+      }else{
+        this.setState({currentUser: userAuth })
+      }
     })
   }
 
--- src/pages/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx
+++ src/pages/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in.component.jsx';
+import SignUp from '../../components/sign-up/sign-up.component.jsx';
+
 import './sign-in-and-sign-out.styles.scss';
 
 const SignInAndSignOut = () => (
     <div className='sign-in-and-sign-out'>
         <SignIn />
+        <SignUp />
     </div>
 )
 
