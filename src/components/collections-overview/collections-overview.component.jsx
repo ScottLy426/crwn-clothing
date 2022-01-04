7d1b3d70f1b24aaeb0745ab1b182055f9aa0b891
scottly426 <lyscott123@gmail.com>
2022-01-03 16:32:32 -0800

    updated directory and shop data with respective selectors, and updated components with new redux flow

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {connect} from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import CollectionsPreview from '../collection-preview/collection-preview.component';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selector';
+
+import './collections-overview.styles.scss';
+
+const CollectionsOverview = ({collections}) => (
+
+    <div className='collections-overview'>
+        {collections.map(({id, ...otherCollectionProps}) => {
+            return <CollectionsPreview key={id} {...otherCollectionProps} />
+        })}
+    </div>
+)
+
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollectionsForPreview
+})
+
+export default connect(mapStateToProps)(CollectionsOverview); 
